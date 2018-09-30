5887c8efbc80fd1bfb9d385386a4c8465ec23c60
Osman Pontes <osmanpontes@gmail.com>
2018-09-30 11:33:49 -0300

    feat(database): create uploads model

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -1,7 +1,9 @@
 const Yards = require('./yards')
 const Employees = require('./employees')
+const Uploads = require('./uploads')
 
 module.exports = {
   Yards,
   Employees,
+  Uploads,
 }
---
+++ src/database/models/uploads.js
@@ -0,0 +1,26 @@
+const { STRING, INTEGER, DATE } = require('sequelize')
+
+module.exports = {
+  create(sequelize) {
+    return sequelize.define('upload', {
+      id: {
+        type: INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      name: {
+        type: STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: DATE,
+        allowNull: false,
+      },
+    }, {})
+  },
+}
