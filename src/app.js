d4b78dba5b8b32f80fe1f00e53591b46f4a2d8f8
Osman Pontes <osmanpontes@gmail.com>
2018-10-01 02:13:15 -0300

    feat(middlewares): add cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const Koa = require('koa')
 const router = require('router')
 const koaBody = require('koa-body')
+const cors = require('koa-cors')
 const db = require('database')
 
 const app = new Koa()
@@ -8,6 +9,9 @@ const app = new Koa()
 app.use(koaBody({
   multipart: true,
 }))
+
+app.use(cors())
+
 app.use(router.routes())
 
 app.context.db = db
